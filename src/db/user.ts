7619c4b62397f3a3e969e8c77244955cc133ef20
Munir Ahmed <elsangedy@gmail.com>
2017-12-24 17:13:35 -0200

    encrypt password  with bycrypt

--- src/db/user.ts
+++ src/db/user.ts
@@ -1,7 +1,12 @@
 import { Document, Schema, Model, model } from 'mongoose'
+
+import { genSaltSync, hashSync, compareSync } from 'bcryptjs'
+
 import { IUser } from '../interfaces/IUser'
 
-export interface IUserModel extends IUser, Document { }
+export interface IUserModel extends IUser, Document {
+  checkPassword(password: string): boolean;
+}
 
 export const UserSchema: Schema = new Schema(
   {
@@ -31,5 +36,18 @@ export const UserSchema: Schema = new Schema(
     timestamps: true
   }
 )
+
+UserSchema.pre('save', function (next) {
+  if (!this.isModified('password')) return next()
+
+  const salt = genSaltSync()
+  this.password = hashSync(this.password, salt)
+
+  next()
+})
+
+UserSchema.methods.checkPassword = function (password: string): Boolean {
+  return compareSync(password, this.password)
+}
 
 export const User: Model<IUserModel> = model<IUserModel>('User', UserSchema)
